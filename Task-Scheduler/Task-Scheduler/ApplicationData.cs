15c117d71986821bb852ab766aa2a5f97d2246ab
JeffHatton <jeahatton@gmail.com>
2015-08-25 23:08:42 -0400

    Added Application Data

---
+++ Task-Scheduler/Task-Scheduler/ApplicationData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Scheduler
+{
+    public class ApplicationData
+    {
+        static ApplicationData data = new ApplicationData();
+
+        public static ApplicationData Get()
+        {
+            return data;
+        }
+
+        public CategoryStore categoryStore;
+        public CalendarItemStore calendarItemStore;
+
+        ApplicationData()
+        {
+            categoryStore = new CategoryStore();
+            calendarItemStore = new CalendarItemStore();
+        }
+
+        public void saveAll()
+        {
+            categoryStore.saveItems();
+            calendarItemStore.saveCalenderItems();
+        }
+
+        public void LoadAll()
+        {
+            calendarItemStore.LoadCalenderItems();
+            categoryStore.LoadCalenderItems();
+        }
+
+    }
+}
